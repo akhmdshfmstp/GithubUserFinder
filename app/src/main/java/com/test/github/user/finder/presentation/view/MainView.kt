43424bd41cff3f6d5f68e685156f84767810d7b9
akhmadmustopo <akhmad.mustopo@metraplasa.co.id>
2020-09-24 11:09:54 +0700

    add main view for main page

---
+++ app/src/main/java/com/test/github/user/finder/presentation/view/MainView.kt
@@ -0,0 +1,16 @@
+package com.test.github.user.finder.presentation.view
+
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.test.github.user.finder.framework.core.view.LifecycleView
+import com.test.github.user.finder.framework.design.LoadingView
+import com.test.github.user.finder.presentation.adapter.UserAdapter
+
+interface MainView : LifecycleView {
+    var retryListener: LoadingView.OnRetryListener
+    var onRefreshListener: SwipeRefreshLayout.OnRefreshListener
+    var layoutManager: LinearLayoutManager
+    var listAdapter: UserAdapter
+    fun onSearchClicked(view: View)
+}
