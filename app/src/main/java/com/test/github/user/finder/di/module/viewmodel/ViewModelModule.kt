83ec4bc09c8fac7e5cdc9abeaf118d5a4c3111ae
akhmadmustopo <akhmad.mustopo@metraplasa.co.id>
2020-09-24 11:10:29 +0700

    add main vm to vm module

--- app/src/main/java/com/test/github/user/finder/di/module/viewmodel/ViewModelModule.kt
+++ app/src/main/java/com/test/github/user/finder/di/module/viewmodel/ViewModelModule.kt
@@ -1,7 +1,15 @@
 package com.test.github.user.finder.di.module.viewmodel
 
+import com.test.github.user.finder.presentation.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
+
+    viewModel {
+        MainViewModel(
+            get()
+        )
+    }
 
 }
