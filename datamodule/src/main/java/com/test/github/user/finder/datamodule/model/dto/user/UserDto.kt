4b3e1fbbe0b72f257b5753572ea2548257f6a55f
akhmadmustopo <akhmad.mustopo@metraplasa.co.id>
2020-09-24 23:36:11 +0700

    remove unused variable

--- datamodule/src/main/java/com/test/github/user/finder/datamodule/model/dto/user/UserDto.kt
+++ datamodule/src/main/java/com/test/github/user/finder/datamodule/model/dto/user/UserDto.kt
@@ -10,9 +10,6 @@ class UserDto : BaseApiDto() {
     @field:Json(name = "total_count")
     var total_count: Int? = null
 
-    @field:Json(name = "incomplete_results")
-    var incomplete_results: Boolean? = null
-
     @field:Json(name = "items")
     var items: List<Items>? = null
 
@@ -24,56 +21,8 @@ class UserDto : BaseApiDto() {
         @field:Json(name = "id")
         var id: Int? = null
 
-        @field:Json(name = "node_id")
-        var node_id: String? = null
-
         @field:Json(name = "avatar_url")
         var avatar_url: String? = null
-
-        @field:Json(name = "gravatar_id")
-        var gravatar_id: String? = null
-
-        @field:Json(name = "url")
-        var url: String? = null
-
-        @field:Json(name = "html_url")
-        var html_url: String? = null
-
-        @field:Json(name = "followers_url")
-        var followers_url: String? = null
-
-        @field:Json(name = "following_url")
-        var following_url: String? = null
-
-        @field:Json(name = "gists_url")
-        var gists_url: String? = null
-
-        @field:Json(name = "starred_url")
-        var starred_url: String? = null
-
-        @field:Json(name = "subscriptions_url")
-        var subscriptions_url: String? = null
-
-        @field:Json(name = "organizations_url")
-        var organizations_url: String? = null
-
-        @field:Json(name = "repos_url")
-        var repos_url: String? = null
-
-        @field:Json(name = "events_url")
-        var events_url: String? = null
-
-        @field:Json(name = "received_events_url")
-        var received_events_url: String? = null
-
-        @field:Json(name = "type")
-        var type: String? = null
-
-        @field:Json(name = "site_admin")
-        var site_admin: Boolean? = null
-
-        @field:Json(name = "score")
-        var score: Double? = null
     }
 
 
