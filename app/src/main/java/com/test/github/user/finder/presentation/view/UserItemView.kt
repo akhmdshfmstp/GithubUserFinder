f295126c0244cf711e9713397d3ac2f6b2274941
akhmadmustopo <akhmad.mustopo@metraplasa.co.id>
2020-09-24 10:02:11 +0700

    create adapter recycler

---
+++ app/src/main/java/com/test/github/user/finder/presentation/view/UserItemView.kt
@@ -0,0 +1,7 @@
+package com.test.github.user.finder.presentation.view
+
+import android.view.View
+
+interface UserItemView {
+    fun onItemClicked(view: View)
+}
---
+++ app/src/main/java/com/test/github/user/finder/presentation/viewmodel/UserItemViewModel.kt
@@ -0,0 +1,5 @@
+package com.test.github.user.finder.presentation.viewmodel
+
+import com.test.github.user.finder.framework.core.base.BaseViewModel
+
+class UserItemViewModel : BaseViewModel()
